4d151eff3fec8f952ed5bf9535c036dd9a765223
Jeongeun-Choi <cje3080@gmail.com>
2023-03-02 23:28:35 +0900

    add: 마크다운을 이용하여 게시글 불러오기(util 함수 추가)

---
+++ lib/posts-utils.ts
@@ -0,0 +1,42 @@
+import fs from "fs";
+import path from "path";
+
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+const getPostData = (fileName: string) => {
+  const filePath = path.join(postsDirectory, fileName);
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const { data, content } = matter(fileContent);
+
+  const postSlug = fileName.replace(/\.md$/, ""); //확장자 없는 파일 이름
+
+  const postData = {
+    slug: postSlug,
+    ...data,
+    content,
+  };
+
+  return postData;
+};
+
+export const getAllPosts = () => {
+  const postFiles = fs.readdirSync(postsDirectory);
+
+  const allPosts: any[] = postFiles.map((postFile) => getPostData(postFile));
+
+  const sortedPosts = allPosts.sort((postA, postB) =>
+    postA.date > postB.date ? -1 : 1
+  );
+
+  return sortedPosts;
+};
+
+export const getFeaturedPosts = () => {
+  const allPosts = getAllPosts();
+
+  const featuredPosts = allPosts.filter((post) => post.isFeatured);
+
+  return featuredPosts;
+};
---
+++ posts/getting-started-with-NextJS.md
@@ -0,0 +1,11 @@
+---
+title: "NextJS 시작해보기"
+date: "2023-03-02"
+image: getting-started-nextjs.png
+excerpt: "첫번째 게시글입니다."
+isFeatured: true
+---
+
+# 제목!!!
+
+구글로 가는 링크입니다 [link](https://google.com)
