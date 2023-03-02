1d34188886f5ca08043025723c2fe47d1e2bbd1c
Jeongeun-Choi <cje3080@gmail.com>
2023-03-03 00:30:10 +0900

    add: 마크다운으로 이미지 렌더링 & 코드 렌더링하기

--- components/Posts/PostDetail/PostContent.tsx
+++ components/Posts/PostDetail/PostContent.tsx
@@ -1,15 +1,56 @@
+import Image from "next/image";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 import { PostDataType } from "../../../lib/posts-utils";
 import classes from "./post-content.module.css";
 import PostHeader from "./PostHeader";
 
 const PostContent = ({ post }: { post: PostDataType }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customRenderers = {
+    // img(image) {
+    //   return <Image src={image.src} alt={image.alt} width={600} height={300} />;
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={image.properties.src}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1];
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- posts/getting-started-with-NextJS.md
+++ posts/getting-started-with-NextJS.md
@@ -1,11 +1,33 @@
 ---
-title: "NextJS 시작해보기"
-date: "2023-03-02"
+title: "Getting Started with NextJS"
+date: "2022-10-16"
 image: getting-started-nextjs.png
-excerpt: "첫번째 게시글입니다."
+excerpt: NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.
 isFeatured: true
 ---
 
-# 제목!!!
+NextJS is a **framework for ReactJS**.
 
-구글로 가는 링크입니다 [link](https://google.com)
+Wait a second ... a "framework" for React? Isn't React itself already a framework for JavaScript?
+
+Well ... first of all, React is a "library" for JavaScript. That seems to be important for some people.
+
+Not for me, but still, there is a valid point: React already is a framework / library for JavaScript. So it's already an extra layer on top of JS.
+
+## Why would we then need NextJS?
+
+Because NextJS makes building React apps easier - especially React apps that should have server-side rendering (though it does way more than just take care of that).
+
+In this article, we'll dive into the core concepts and features NextJS has to offer:
+
+- File-based Routing
+- Built-in Page Pre-rendering
+- Rich Data Fetching Capabilities
+- Image Optimization
+- Much More
+
+## File-based Routing
+
+![Create routes via your file + folder structure](/images/posts/getting-started-with-NextJS/nextjs-file-based-routing.png)
+
+... More content ...
