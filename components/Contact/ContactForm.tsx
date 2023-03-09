27fc8962ab1f93c37e26e9923c6d048b0165aa13
Jeongeun-Choi <cje3080@gmail.com>
2023-03-10 00:04:49 +0900

    add: 알림창 추가

--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -1,65 +1,126 @@
-import { FormEvent, useRef } from 'react';
-import classes from './contact-form.module.css';
+import { FormEvent, useEffect, useRef, useState } from "react";
+import Notification from "../ui/Notification";
+import classes from "./contact-form.module.css";
+
+async function sendContactData(contact: {
+  email: string;
+  name: string;
+  message: string;
+}) {
+  const response = await fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(contact),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+}
 
 const ContactForm = () => {
-    const emailRef = useRef<HTMLInputElement>(null);
-    const nameRef = useRef<HTMLInputElement>(null);
-    const messageRef = useRef<HTMLTextAreaElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
 
-    const handleSubmitMessage = (event: FormEvent) => {
-        event.preventDefault();
+  const [requestStatus, setRequestStatus] = useState<string>("");
+  const [requestError, setRequestError] = useState<string>("");
 
-        const email = emailRef.current?.value;
-        const name = nameRef.current?.value;
-        const message = messageRef.current?.value;
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus("");
+        setRequestError("");
+      }, 3000);
 
-        if(!email || !name || !message) {
-            return;
-        }
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
 
-        const params = {email, name, message};
+  const handleSubmitMessage = async (event: FormEvent) => {
+    event.preventDefault();
 
-        fetch('/api/contact', {method: 'POST', body: JSON.stringify(params), headers: {'Content-Type': 'application/json'}})
+    const email = emailRef.current?.value;
+    const name = nameRef.current?.value;
+    const message = messageRef.current?.value;
+
+    if (!email || !name || !message) {
+      return;
     }
 
-    return ( <section className={classes.contact}>
-        <h1>How can I help you?</h1>
-        <form className={classes.form} onSubmit={handleSubmitMessage}>
-          <div className={classes.controls}>
-            <div className={classes.control}>
-              <label htmlFor='email'>Your Email</label>
-              <input
-              ref={emailRef}
-                type='email'
-                id='email'
-                required
-              />
-            </div>
-            <div className={classes.control}>
-              <label htmlFor='name'>Your Name</label>
-              <input
-              ref={nameRef}
-                type='text'
-                id='name'
-                required
-              />
-            </div>
+    const params = { email, name, message };
+    setRequestStatus("pending");
+    try {
+      await sendContactData(params);
+      setRequestStatus("success");
+      emailRef.current.value = "";
+      nameRef.current.value = "";
+      messageRef.current.value = "";
+    } catch (e: any) {
+      setRequestStatus("error");
+      setRequestError(e.message);
+    }
+  };
+
+  let notification;
+
+  if (requestStatus === "pending") {
+    notification = {
+      status: "pending",
+      title: "메시지 전송 중...",
+      message: "메시지를 전송하고 있습니다.",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notification = {
+      status: "success",
+      title: "메시지 전송 성공",
+      message: "메시지가 전송 되었습니다.",
+    };
+  }
+
+  if (requestStatus === "error") {
+    notification = {
+      status: "error",
+      title: "메시지 전송 실패",
+      message: requestError,
+    };
+  }
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you?</h1>
+      <form className={classes.form} onSubmit={handleSubmitMessage}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input ref={emailRef} type="email" id="email" required />
           </div>
           <div className={classes.control}>
-            <label htmlFor='message'>Your Message</label>
-            <textarea
-            ref={messageRef}
-              id='message'
-              rows={5}
-              required
-            ></textarea>
+            <label htmlFor="name">Your Name</label>
+            <input ref={nameRef} type="text" id="name" required />
           </div>
-  
-          <div className={classes.actions}>
-            <button>Send Message</button>
-          </div>
-        </form>
-      </section>)
-}
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="message">Your Message</label>
+          <textarea ref={messageRef} id="message" rows={5} required></textarea>
+        </div>
+
+        <div className={classes.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+    </section>
+  );
+};
 
 export default ContactForm;
---
+++ components/ui/Notification.tsx
@@ -0,0 +1,30 @@
+import classes from './notification.module.css';
+
+type NotificationType = {
+  title: string;
+  message: string;
+  status: string;
+}
+
+function Notification({title, message, status}: NotificationType) {
+  let statusClasses = '';
+
+  if (status === 'success') {
+    statusClasses = classes.success;
+  }
+
+  if (status === 'error') {
+    statusClasses = classes.error;
+  }
+
+  const cssClasses = `${classes.notification} ${statusClasses}`;
+
+  return (
+    <div className={cssClasses}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default Notification;
