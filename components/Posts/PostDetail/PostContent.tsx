7b716632dbcfbab0c1724ef324ad43d0be58fcc3
Jeongeun-Choi <cje3080@gmail.com>
2023-03-13 22:39:37 +0900

    add: 배포 전 사전 정리

--- components/Posts/PostDetail/PostContent.tsx
+++ components/Posts/PostDetail/PostContent.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
 
 import { PostDataType } from "../../../lib/posts-utils";
 import classes from "./post-content.module.css";
 import PostHeader from "./PostHeader";
+
+SyntaxHighlighter.registerLanguage("css", css);
+SyntaxHighlighter.registerLanguage("js", js);
 
 const PostContent = ({ post }: { post: PostDataType }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
@@ -41,6 +46,7 @@ const PostContent = ({ post }: { post: PostDataType }) => {
         <SyntaxHighlighter
           style={atomDark}
           language={language}
+          // eslint-disable-next-line react/no-children-prop
           children={children}
         />
       );
--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,38 +2,48 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { MongoClient } from "mongodb";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if(req.method === 'POST') {
-        const {email, name, message} = req.body;
-        
-        if(!email || !name || !message || !email.includes('@') || (name.trim() === '') || (message.trim() === '')){
-            return res.status(422).json({message: '유효하지 않은 입력입니다.'});
-        }
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
 
-        const newMessage = {
-            email, name, message
-        }
-
-        let client = null;
-
-        try {
-            client = await MongoClient.connect(`mongodb+srv://jeong:${process.env.MONGODB_PWD}@cluster0.bez8tll.mongodb.net/test`)
-        } catch(error) {
-            res.status(500).json({message: 'Could not Connect DB'})
-            return 
-        }
-
-        const db = client.db();
-
-        try {
-            const result = await db.collection('messages').insertOne(newMessage);
-            newMessage.id = result.insertedId;
-        } catch(e) {
-            res.status(500).json({message: "Storing message fail!"});
-            return;
-        }
-        client.close();
-        return res.status(200).json(newMessage);
+    if (
+      !email ||
+      !name ||
+      !message ||
+      !email.includes("@") ||
+      name.trim() === "" ||
+      message.trim() === ""
+    ) {
+      return res.status(422).json({ message: "유효하지 않은 입력입니다." });
     }
+
+    const newMessage: { [key: string]: string } = {
+      email,
+      name,
+      message,
+    };
+
+    let client = null;
+    const connectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PWD}@${process.env.MONGODB_CLUSTER}.bez8tll.mongodb.net/${process.env.MONGODB_DB}`;
+    try {
+      client = await MongoClient.connect(connectionString);
+    } catch (error) {
+      res.status(500).json({ message: "Could not Connect DB" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+
+      newMessage.id = result.insertedId;
+    } catch (e) {
+      res.status(500).json({ message: "Storing message fail!" });
+      return;
+    }
+    client.close();
+    return res.status(200).json(newMessage);
+  }
 }
 
 export default handler;
